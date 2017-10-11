a0e892c3d7b55ebac91f3cc434ffaca7a95684c7
rocket-star <rudy.fernando@hotmail.fr>
2017-10-12 00:03:37 +0200

    Ajout des commentaires

--- Sprint1/Source1.c
+++ Sprint1/Source1.c
@@ -1,89 +1,64 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define max_champs 25
-#define lgMot 30
-#define lgMax 80
+#define max_champs 25// Nombre maximum de champs d'une table
+#define lgMot 30// Longueur maximale d'une chaine de caractères
+#define lgMax 80// Longueur maximale d'une ligne de commande
 #pragma warning (disable:4996)
 
-
-
 typedef struct {
-	char nom[lgMot + 1];
-	char type[lgMot + 1];
+	char nom[lgMot + 1];// nom du champ
+	char type[lgMot + 1];// type du champ
 }Champ;
 
-
 typedef struct {
-	char nom[lgMot + 1];
-	Champ schema[max_champs];
+	char nom[lgMot + 1];// nom de la table
+	Champ schema[max_champs];//schema tableau statique sur-dimensionné à max_champs
 	unsigned char nbChamps;// nombre de champs de la table
 }Table;
 
-
-
-
-
-
-
-
-void create_table(Table* t) {
-	char mot[lgMot + 1];
-	if (strcmp(t->nom, "") == 0) {
-		scanf("%s", &mot);
-		strcpy(t->nom, &mot);
-		printf("Entrez un nombre de champs");
-		scanf("%s", mot);
-		t->nbChamps = atoi(mot);
-		for (unsigned int i = 0; i < t->nbChamps; ++i) {
-			printf("Nom du champs n°%d", i);
-			scanf("%s", &mot);
-			strcpy(t->schema[i].nom, &mot);
-			printf("Type du champs n°%d", i);
-			scanf("%s", &mot);
-			strcpy(t->schema[i].type, &mot);
-		}
-	}
-	else {
-		printf("Table Existante\n");
-	}
-	return;
-
-}
-
-void afficher_schema(Table*t) {
-	char mot[lgMot + 1];
-	scanf("%s", mot);
-
-	if (strcmp(mot, t->nom) == 0) {
-		printf("%s %d\n", t->nom, t->nbChamps);
-		for (unsigned int i = 0; i < t->nbChamps; ++i) {
-			printf("%s %s", t->schema[i].nom, t->schema[i].type);
-		}
-	}
-	else {
-		printf("Table Inconnu\n");
-	}
-	return;
-}
+void afficher_schema(Table* t);// Fonction afficher_schema
+void create_table(Table* t);// Fonction create_table
 
 int main() {
 
-	char mot[lgMot + 1];
+	char mot[lgMot + 1];// +1 pour stocker le caractère '\0'
 	Table t;
-	strcpy(t.nom, "");
-	Champ c;
 	while (1) {
-		printf("Entrer une commande");
-		scanf("%s", &mot);
+		//printf("Entrer une commande");
+		scanf("%s", mot);// Récuperation de la commande
 
 		if (strcmp(mot, "Create_table") == 0) {
-			create_table(&t);
+			create_table(&t);//appel de la fonction create_table
 		}
-		if (strcmp(mot, "Afficher_schema") == 0) {
-			afficher_schema(&t);
-
+		else if (strcmp(mot, "Afficher_schema") == 0) {
+			afficher_schema(&t);//appel de la fonction afficher_schema
+		}
+		else if (strcmp(mot, "Exit") == 0) {
+			exit(0); // sortie du programme principal 
 		}
 	}
-	system("pause");
+}
+void create_table(Table* t) {
+	char temp[lgMot + 1];
+	scanf("%s", &temp);// Récupération nom de la table
+	if (strcmp(temp,t->nom)!=0){	
+		strcpy(t->nom, temp);
+		scanf("%s", &(t->nbChamps));// Récuperation nombre de champs 
+		for (unsigned int i = 0; i < atoi(&(t->nbChamps)); ++i)
+			scanf("%s %s", t->schema[i].nom, t->schema[i].type);// Récupération de chaque champ
+	}
+	else
+		printf("table existante\n");
+}
+void afficher_schema(Table* t) {
+	char mot[lgMot + 1];
+	scanf("%s", mot);// Récupération nom de la table
+	if (strcmp(mot, t->nom) == 0) {
+		printf("%s %s\n", &t->nom, &t->nbChamps);
+		for (unsigned int i = 0; i < atoi(&(t->nbChamps)); ++i)
+			printf("%s %s\n", t->schema[i].nom, t->schema[i].type);// Affichage champs
+	}
+	else 
+		printf("table inconnue\n");
 }
